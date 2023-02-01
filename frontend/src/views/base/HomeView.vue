649cbea8d003c156725c4bb3e448b000981ff5b7
Victor Magalhaes <victor.barreto@buser.com.br>
2023-02-01 17:34:39 -0300

    Iniciando projeto audio-player (setup)

---
+++ frontend/src/views/base/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container class="fill-height">
+    <v-responsive class="d-flex align-center text-center fill-height">
+      <audio src="./shakira.mp3" type="audio/mpeg" autoplay></audio>
+    </v-responsive>
+  </v-container>
+</template>
+
+<script>
+import { mapState } from "pinia"
+import { useAccountsStore } from "@/stores/accountsStore"
+
+export default {
+  computed: {
+    ...mapState(useAccountsStore, ["loggedUser"]),
+  },
+}
+</script>
