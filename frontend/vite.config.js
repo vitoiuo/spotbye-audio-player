f3e71b6fdcf33890512f9c27edb2b2badf2aa737
Victor Magalhaes <victor.barreto@buser.com.br>
2023-02-09 16:31:24 -0300

    mirage config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,27 @@
 // Plugins
 import vue from "@vitejs/plugin-vue"
 import vuetify, { transformAssetUrls } from "vite-plugin-vuetify"
-
+import { createServer } from "miragejs"
 // Utilities
 import { defineConfig } from "vite"
 import { fileURLToPath, URL } from "node:url"
+
+console.log('AAAAAAAAAAAAAAAAAAAA' + process.env.DEVELOPMENT_MODE)
+if (process.env.DEVELOPMENT_MODE === 'mock') {
+  createServer({
+    routes() {
+      this.namespace = "api"
+      this.urlPrefix = 'http://localhost:8000/';
+      this.get("/musics/list_songs", () => {
+        return {
+          songs: [
+            { id: 1, title: "Bump of Galinha", artist:"Bump of Chicken", cover: "https://images.genius.com/8d12300f2776f719bd9f1a7f4a2354e9.1000x1000x1.jpg" },
+          ],
+        }
+      })
+    },
+  })
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
